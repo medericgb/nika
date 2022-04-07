902c4b677113dedff6f010de383d79cc7e06765e
Médéric Gbagba <medericjoel@gmail.com>
2022-04-07 11:27:24 +0000

    DONE: LineItem Spec

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -15,14 +15,32 @@ class LineItem < ApplicationRecord
   end
 
   def sub_total
-    sub = O
-    self.quantity * self.product.price 
+    sub = 0
     if self.product.code == "GR1"
-      if self.quantity % 2 != 
+      if self.quantity > 1
+        if self.quantity % 2 == 0
+          sub = (self.quantity / 2) * self.product.price
+        elsif self.quantity % 2 == 1
+          sub = ((self.quantity / 2) * self.product.price) + 3.11
+        end
+      else
+        sub = 3.11
+      end
     end
     if self.product.code == "SR1" 
+      if self.quantity >= 3
+        sub = (self.quantity * self.product.price) - (self.quantity * 0.5)  
+      else
+        sub = self.quantity * self.product.price 
+      end
     end
     if self.product.code == "CF1" 
+      if self.quantity >= 3
+        sub = (self.quantity * self.product.price * 2) / 3  
+      else
+        sub = self.quantity * self.product.price 
+      end
     end  
+    return sub
   end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe LineItem, type: :model do
   let(:cart) { FactoryBot.create(:cart) }
   let(:product) { FactoryBot.create(:product) }
+  let(:grn) { FactoryBot.create(:product, code: "GR1", name: "Green", price: 3.11) }
+  let(:str) { FactoryBot.create(:product, code: "SR1", name: "Strawberries", price: 5.00) }
+  let(:cff) { FactoryBot.create(:product, code: "CF1", name: "Coffee", price: 11.23) }
   let(:line_item) { FactoryBot.create(:line_item, product_id: product.id, cart_id: cart.id) }
   
   describe ".add_quantity" do
@@ -27,6 +30,81 @@ RSpec.describe LineItem, type: :model do
       it "quantity is equal to 1" do
         line_item.reduce_quantity
         expect(line_item.quantity).to eq 1
+      end
+    end
+  end
+
+  describe ".sub_total" do
+    context "when cart contain one GR1" do
+      it "line item sub price is equal to one product price" do
+        cart.add_product(grn)
+        item = cart.line_items.find { |item| item.product_id == grn.id }
+        expect(item.sub_total).to eq(grn.price)
+      end
+    end
+
+    context "when cart contain pair GR1" do
+      it "line item sub price is equal to half price of all product" do
+        cart.add_product(grn)
+        cart.add_product(grn)
+        item = cart.line_items.find { |item| item.product_id == grn.id }
+        expect(item.sub_total).to eq(grn.price * (item.quantity / 2))
+      end
+    end
+
+    context "when cart contain impair GR1" do
+      it "line item sub price is equal to half price of all product" do
+        cart.add_product(grn)
+        cart.add_product(grn)
+        cart.add_product(grn)
+        item = cart.line_items.find { |item| item.product_id == grn.id }
+        expect(item.sub_total).to eq(((item.quantity / 2) * grn.price) + 3.11)
+      end
+    end
+    
+    context "when cart contain less or more than 3 SR1" do
+      it "sub price equal to 5.00 if qty is one" do
+        cart.add_product(str)
+        item = cart.line_items.find { |item| item.product_id == str.id }
+        expect(item.sub_total).to eq(5.00)
+      end
+
+      it "sub price equal to 10.0 if qty is two" do
+        cart.add_product(str)
+        cart.add_product(str)
+        item = cart.line_items.find { |item| item.product_id == str.id }
+        expect(item.sub_total).to eq(str.price * 2)
+      end
+
+      it "sub price equal to if qty is equal or more than 3 minus 0.5 by product" do
+        cart.add_product(str)
+        cart.add_product(str)
+        cart.add_product(str)
+        item = cart.line_items.find { |item| item.product_id == str.id }
+        expect(item.sub_total).to eq((str.price * item.quantity) - (item.quantity * 0.5))
+      end
+    end
+
+    context "when cart contain less or more than 3 CF1" do
+      it "sub price is 11.23 if qty is 1" do
+        cart.add_product(cff)
+        item = cart.line_items.find { |item| item.product_id == cff.id }
+        expect(item.sub_total).to eq(cff.price)
+      end
+
+      it "sub price is 11.23 * 2 if qty is 2" do
+        cart.add_product(cff)
+        cart.add_product(cff)
+        item = cart.line_items.find { |item| item.product_id == cff.id }
+        expect(item.sub_total).to eq(cff.price * 2)
+      end
+
+      it "sub price is (11.23 * qty) * 2/3 if qty is equal or more than 3" do
+        cart.add_product(cff)
+        cart.add_product(cff)
+        cart.add_product(cff)
+        item = cart.line_items.find { |item| item.product_id == cff.id }
+        expect(item.sub_total).to eq(((cff.price * item.quantity * 2) / 3))
       end
     end
   end
